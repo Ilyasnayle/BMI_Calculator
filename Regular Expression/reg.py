4e22dd3d2576d584138ef75b5cf56148f3aea29d
Ilyas Nayle <57596414+Ilyasnayle@users.noreply.github.com>
2024-04-22 20:39:30 +0300

    Update reg.py

--- Regular Expression/reg.py
+++ Regular Expression/reg.py
@@ -1,3 +1,12 @@
+#Exercise: Extracting email addresses from text
+
+#Given a text below,write a regular expression pattern to extract all emails from a text.
+
+#email_text = "Contact us at support@example.com or at helpdesk@mysite.org."
+
+#Expected Output ['support@example.com', 'helpdesk@mysite.org']
+
+
 import re
 
 email = "Contact us at support@example.com or at helpdesk@mysite.org."
